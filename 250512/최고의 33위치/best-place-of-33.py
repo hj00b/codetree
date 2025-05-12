f5d3c67e3ed4418350b3b2f5e7af4a140d57ea18
hj <hjgit00b@gmail.com>
2025-05-12 22:39:32 +0900

    Title: 최고의 33위치, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250512/최고의 33위치/best-place-of-33.py
+++ 250512/최고의 33위치/best-place-of-33.py
@@ -3,12 +3,9 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
 ans = 0
-for k in range(3):
-    for m in range(3):
-        ans += grid[k][m]
 
-for i in range(0, n - 3):
-    for j in range(0, n - 3):
+for i in range(0, n - 2):
+    for j in range(0, n - 2):
         total = 0
         for k in range(3):
             for m in range(3):
